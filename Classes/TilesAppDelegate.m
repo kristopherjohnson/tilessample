040e40b68725d32cfda13a630919e7c33e576d04
Kristopher Johnson <kris@kristopherjohnson.net>
2010-02-05 21:49:12 -0500

    Remove unneeded comment

--- Classes/TilesAppDelegate.m
+++ Classes/TilesAppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     
-    // Override point for customization after app launch    
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
 	
